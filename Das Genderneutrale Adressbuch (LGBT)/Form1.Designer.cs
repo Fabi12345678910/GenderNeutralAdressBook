78b1530b57ceeb3733073bcc13635daa52b821d9
Nicosponk <50570062+Nicosponk@users.noreply.github.com>
2019-05-27 17:46:25 +0200

    Update Form1.Designer.cs

--- Das Genderneutrale Adressbuch (LGBT)/Form1.Designer.cs
+++ Das Genderneutrale Adressbuch (LGBT)/Form1.Designer.cs
@@ -677,9 +677,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(195, 12);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.Size = new System.Drawing.Size(27, 13);
             this.label3.TabIndex = 72;
-            this.label3.Text = "plz";
+            this.label3.Text = "PLZ";
             // 
             // label2
             // 
